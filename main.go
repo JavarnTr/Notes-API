e760bd5d269e3cc0830b8a229510e777973adf08
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2022-11-06 22:18:22 +1300

    broken

--- main.go
+++ main.go
@@ -28,6 +28,15 @@ type Note struct {
 	Time       string
 }
 
+type AddNote struct {
+	Name       string
+	Text       string
+	Status     string
+	Delegation string
+	Userid     string
+	Time       string
+}
+
 type User struct {
 	UserID string
 	Name   string
@@ -35,12 +44,6 @@ type User struct {
 
 func main() {
 
-	edit()
-
-	http.ListenAndServe(":8082", nil)
-}
-
-func add() {
 	tmpl := template.Must(template.ParseFiles("forms/forms.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -48,18 +51,6 @@ func add() {
 			tmpl.Execute(w, nil)
 			return
 		}
-
-		details := Note{
-			Name:       r.FormValue("name"),
-			Text:       r.FormValue("text"),
-			Status:     r.FormValue("status"),
-			Delegation: r.FormValue("delegation"),
-			Userid:     r.FormValue("userid"),
-			Time:       r.FormValue("time"),
-		}
-
-		// do something with details
-		_ = details
 
 		//Connect to the database
 		connStr := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -71,9 +62,21 @@ func add() {
 		}
 		defer db.Close()
 
+		//-------------------------------Add Note-------------------------------//
+		details := AddNote{
+			Name:       r.FormValue("addName"),
+			Text:       r.FormValue("addText"),
+			Status:     r.FormValue("addStatus"),
+			Delegation: r.FormValue("addDelegation"),
+			Userid:     r.FormValue("addUserID"),
+			Time:       r.FormValue("addTime"),
+		}
+
+		// do something with details
+		_ = details
+
 		//Insert the note data into the Postgres database for storage
-		insertStatement := `INSERT INTO notes (name, text, status, delegation, userid, date) 
-		VALUES ($1, $2, $3, $4, $5, $6)`
+		insertStatement := `INSERT INTO notes (name, text, status, delegation, userid, date) VALUES ($1, $2, $3, $4, $5, $6)`
 		_, err = db.Exec(insertStatement, details.Name, details.Text, details.Status, details.Delegation, details.Userid, details.Time)
 		if err != nil {
 			panic(err)
@@ -81,35 +84,13 @@ func add() {
 			fmt.Println("\nRow inserted successfully!")
 		}
 
-		tmpl.Execute(w, struct{ Success bool }{true})
-	})
-}
-
-func remove() {
-	tmpl := template.Must(template.ParseFiles("forms/forms.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-
+		//-------------------------------Remove Note-------------------------------//
 		removeDetails := Note{
-			NoteID: r.FormValue("removeNoteID"),
+			NoteID: r.FormValue("deleteNoteID"),
 		}
 
 		// do something with details
 		_ = removeDetails
-
-		//Connect to the database
-		connStr := fmt.Sprintf("host=%s port=%d user=%s "+
-			"password=%s dbname=%s sslmode=disable",
-			host, port, user, password, dbname)
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			panic(err)
-		}
-		defer db.Close()
 
 		//Insert the note data into the Postgres database for storage
 		deleteStatement := `delete from notes where id = $1`
@@ -121,7 +102,10 @@ func remove() {
 		}
 
 		tmpl.Execute(w, struct{ Success bool }{true})
+
 	})
+
+	http.ListenAndServe(":8080", nil)
 }
 
 func createUser() {
